ca5360beeb20593b08f9174e4fee19eabfbda573
Zahangir Alam <zahangirdu1@gmail.com>
2023-01-04 13:23:57 +0600

    init

---
+++ forLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(i+1);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
