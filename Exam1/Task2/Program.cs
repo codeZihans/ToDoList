b1b484ec371c3e6e9d62ca570344cfe4ea61e6a0
Zahangir Alam <zahangirdu1@gmail.com>
2024-01-28 13:56:09 +0600

    task two completed.

--- Exam1/Task2/Program.cs
+++ Exam1/Task2/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Task 2");
+
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+number << 3;
+
+string binary = Convert.ToString(number, 2);
+
+
+Console.WriteLine(binary);
